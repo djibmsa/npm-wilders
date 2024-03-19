b0c41571f82896149e8b59a4378f8af741193215
djibmsa@gmail.com <djibmsa@gmail.com>
2024-03-19 12:01:04 +0100

    nmp ateliers

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+console.log(chalk.yellow('Stay focus and stay hard!!!'));
+console.log(chalk.green('Miraq'));
+console.log(chalk.red('Max'));
+console.log(chalk.yellow('Rostom'));
+console.log(chalk.blue('Facron'));
+
